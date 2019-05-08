7b45b3409b773e8970af1cf75a3d00f95f78e2dd
Sumayya-Syeda <sumayya.syeda@gmail.com>
2019-05-07 22:43:41 -0700

    add class start

---
+++ app/src/main/java/com/example/school/cougarcalendar/AddClassActivityActivity.java
@@ -0,0 +1,13 @@
+package com.example.school.cougarcalendar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddClassActivityActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_class_activity);
+    }
+}
--- app/src/main/java/com/example/school/cougarcalendar/assignments.java
+++ app/src/main/java/com/example/school/cougarcalendar/assignments.java
@@ -2,12 +2,28 @@ package com.example.school.cougarcalendar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
 
 public class assignments extends AppCompatActivity {
-
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_assignments);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openAddClasses();
+            }
+        });
+    }
+
+    public void openAddClasses(){
+        Intent intent = new Intent(this, AddClassActivityActivity.class);
+        startActivity(intent);
     }
 }
